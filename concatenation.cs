fed0de17c9caac26830708d20815f3c01cae543b
aLokesh01 <lokesh.ajw@gmail.com>
2025-06-13 20:13:32 +0530

    Add files via upload

---
+++ concatenation.cs
@@ -0,0 +1,8 @@
+string nm =  "lokesh";
+int a = 2;
+int b = 5;
+int c = 1;
+int d = 3;
+int e = 4;
+Console.Write($"Mobile num of {nm} is 996{b}{b}88{e}{d}{e}");
+Console.Write("\nMobile num of " + nm + " is" + " 996" + b + b + "88" + e + d + e);
